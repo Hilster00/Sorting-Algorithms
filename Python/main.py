e6663dd0f36bd032df87740cc410fe05358433cd
Hilster Barbosa Santos <69377592+Hilster00@users.noreply.github.com>
2022-09-23 01:05:37 -0300

    Create main.py

---
+++ Python/main.py
@@ -0,0 +1,33 @@
+import random
+import QuickSort
+import InsertionSort
+import MergeSort
+
+vetores=[[],[],[]]
+tamanho=7
+
+#adiciona valores em ordem aleatoria a lista
+for i in range(0,tamanho):
+    #adiciona valores as sublistas
+    vetores[0].append(random.randint(0,tamanho))
+    vetores[1].append(random.randint(0,tamanho))
+    vetores[2].append(random.randint(0,tamanho))
+    
+    
+
+#onrdenando vetores
+
+#ordenando sublista 0 pelo metodo InsertionSort
+print(f"Vetor desordenado:{vetores[0]}")
+InsertionSort.InsertionSort(vetores[0])
+print(f"Vetor ordenado InsertionSort:{vetores[0]}")
+
+#ordenando sublista 0 pelo metodo MergeSort
+print(f"Vetor desordenado:{vetores[1]}")
+MergeSort.MergeSort(vetores[1])
+print(f"Vetor ordenado MergeSort:{vetores[1]}")
+
+#ordenando sublista 0 pelo metodo QuickSort
+print(f"Vetor desordenado:{vetores[2]}")
+QuickSort.QuickSort(vetores[2])
+print(f"Vetor ordenado QuicSort:{vetores[2]}")
