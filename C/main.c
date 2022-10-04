342478605313fa71ca9732f3b9d4d4aba79b2b68
Hilster Barbosa Santos <69377592+Hilster00@users.noreply.github.com>
2022-10-04 16:37:18 -0300

    Update main.c

--- C/main.c
+++ C/main.c
@@ -1,19 +1,33 @@
 #include <stdio.h>
 #include "InsertSort.h"
+#include "QuickSort.h"
 
 void main(){
     
-    int vetor[6];
+    int vetor1[6],vetor2[6];
     for(int i=5;i>=0;i--){
-        vetor[5-i]=i;
+        vetor1[5-i]=i;
+        vetor2[5-i]=i;
     }
+    printf("InsertSort\n");
     printf("vetor:");
     for(int i=0;i<6;i++){
-        printf("%i,",vetor[i]);
+        printf("%i,",vetor1[i]);
     }
-    InsertSort(&vetor[2],3);
-    printf("\nvetor:");
+    InsertSort(&vetor1[0],6);
+    printf("\nvetor ordenado:");
     for(int i=0;i<6;i++){
-        printf("%i,",vetor[i]);
+        printf("%i,",vetor1[i]);
+    }
+    
+    printf("\nQuickSort\n");
+    printf("vetor:");
+    for(int i=0;i<6;i++){
+        printf("%i,",vetor2[i]);
+    }
+    QuickSort(&vetor2[0],0,6);
+    printf("\nvetor ordenado:");
+    for(int i=0;i<6;i++){
+        printf("%i,",vetor2[i]);
     }
 }
