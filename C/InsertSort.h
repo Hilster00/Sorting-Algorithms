7236dab5b1574c6f4e964d739cb069cd340bfe04
Hilster Barbosa Santos <69377592+Hilster00@users.noreply.github.com>
2022-10-05 22:45:07 -0300

    Update InsertSort.h

--- C/InsertSort.h
+++ C/InsertSort.h
@@ -1,16 +1,14 @@
 void InsertSort(int *vetor,int fim){
     int i,j;
-    int *temp,*anterior;
+    int *temp;
     for(i=1;i<fim+1;i++){
         j=i;
         temp=vetor+i;
-        anterior=temp-1;
-        while(j>0 && *temp<*anterior){
-            *temp=*temp+*anterior;
-            *anterior=*temp-*anterior;
-            *temp=*temp-*anterior;
+        while(j>0 && *temp<*(temp - 1)){
+            *temp=*temp+*(temp - 1);
+            *(temp - 1)=*temp-*(temp - 1);
+            *temp=*temp-*(temp - 1);
             temp--;
-            anterior--;
             j--;
         }
     }
