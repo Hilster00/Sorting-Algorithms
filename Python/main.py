200b7f08a9307e95f5bdb12e21d4ee1337628f10
Hilster Barbosa Santos <69377592+Hilster00@users.noreply.github.com>
2022-10-07 14:41:51 -0300

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -2,8 +2,9 @@ import random
 import QuickSort
 import InsertSort
 import MergeSort
+import SelectSort
 
-vetores=[[],[],[]]
+vetores=[[],[],[],[]]
 tamanho=7
 
 #adiciona valores em ordem aleatoria a lista
@@ -12,6 +13,7 @@ for i in range(0,tamanho):
     vetores[0].append(random.randint(0,tamanho))
     vetores[1].append(random.randint(0,tamanho))
     vetores[2].append(random.randint(0,tamanho))
+    vetores[3].append(random.randint(0,tamanho))
 
 #onrdenando vetores
 
@@ -20,12 +22,18 @@ print(f"Vetor desordenado:{vetores[0]}")
 InsertSort.InsertSort(vetores[0])
 print(f"Vetor ordenado InsertSort:{vetores[0]}")
 
-#ordenando sublista 0 pelo metodo MergeSort
+#ordenando sublista 0 pelo metodo SelectSort
 print(f"Vetor desordenado:{vetores[1]}")
-MergeSort.MergeSort(vetores[1])
-print(f"Vetor ordenado MergeSort:{vetores[1]}")
+SelectSort.SelectSort(vetores[1])
+print(f"Vetor ordenado SelectSort:{vetores[1]}")
+
+
+#ordenando sublista 0 pelo metodo MergeSort
+print(f"Vetor desordenado:{vetores[2]}")
+MergeSort.MergeSort(vetores[2])
+print(f"Vetor ordenado MergeSort:{vetores[2]}")
 
 #ordenando sublista 0 pelo metodo QuickSort
-print(f"Vetor desordenado:{vetores[2]}")
-QuickSort.QuickSort(vetores[2])
-print(f"Vetor ordenado QuicSort:{vetores[2]}")
+print(f"Vetor desordenado:{vetores[3]}")
+QuickSort.QuickSort(vetores[3])
+print(f"Vetor ordenado QuicSort:{vetores[3]}")
