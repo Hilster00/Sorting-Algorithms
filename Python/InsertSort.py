c1fd7cb000cd85eeeed348626dca81b86cf4d914
Hilster Barbosa Santos <69377592+Hilster00@users.noreply.github.com>
2022-10-07 14:29:59 -0300

    Update InsertSort.py

--- Python/InsertSort.py
+++ Python/InsertSort.py
@@ -1,12 +1,13 @@
-def InsertSort(vetor):
-    for i in range(0,len(vetor)):
+def InsertSort(vetor,inicio=0,fim=None):
+    if fim == None:
+        fim=len(vetor)
+    for i in range(inicio,fim):
         key=vetor[i]#guarda vetor[i] em uma chave
         
         #move valores > key para a direita
-        for j in range(i,-1,-1):
+        for j in range(i,inicio-1,-1):
             if key>=vetor[j-1]:#interrompe o laco quando encontra valor menor que key
                 break
             vetor[j]=vetor[j-1]#move valor para a direita se vetor[j-1]>key
-        
         #coloca valor da chave no local correto
         vetor[j]=key
