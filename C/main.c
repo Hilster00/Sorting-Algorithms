e48496f6145efc2801425dcef84852c3a2d1491a
Hilster Barbosa Santos <69377592+Hilster00@users.noreply.github.com>
2022-10-08 17:49:31 -0300

    Update main.c

--- C/main.c
+++ C/main.c
@@ -3,15 +3,17 @@
 #include "QuickSort.h"
 #include "MergeSort.h"
 #include "SelectSort.h"
+#include "BubbleSort.h"
 
 void main(){
     
-    int vetor1[6],vetor2[6],vetor3[6],vetor4[6];
+    int vetor1[6],vetor2[6],vetor3[6],vetor4[6],vetor5[6];
     for(int i=5;i>=0;i--){
         vetor1[5-i]=i;
         vetor2[5-i]=i;
         vetor3[5-i]=i;
         vetor4[5-i]=i;
+        vetor5[5-i]=i;
     }
     
     printf("InsertSort\n");
@@ -25,36 +27,47 @@ void main(){
         printf("%i,",vetor1[i]);
     }
     
-    printf("SelectSort\n");
+    printf("\nSelectSort\n");
     printf("vetor:");
     for(int i=0;i<6;i++){
         printf("%i,",vetor2[i]);
     }
-    SelectSort(&vetor2[0],5);
+    SelectSort(&vetor2[0],0,5);
     printf("\nvetor ordenado:");
     for(int i=0;i<6;i++){
         printf("%i,",vetor2[i]);
+    }
+    
+    printf("\nBubbleSort\n");
+    printf("vetor:");
+    for(int i=0;i<6;i++){
+        printf("%i,",vetor3[i]);
+    }
+    BubbleSort(&vetor3[0],0,5);
+    printf("\nvetor ordenado:");
+    for(int i=0;i<6;i++){
+        printf("%i,",vetor3[i]);
     }
     
     printf("\nQuickSort\n");
     printf("vetor:");
     for(int i=0;i<6;i++){
-        printf("%i,",vetor3[i]);
+        printf("%i,",vetor4[i]);
     }
-    QuickSort(&vetor3[0],0,5);
+    QuickSort(&vetor4[0],0,5);
     printf("\nvetor ordenado:");
     for(int i=0;i<6;i++){
-        printf("%i,",vetor3[i]);
+        printf("%i,",vetor4[i]);
     }
     
     printf("\nMergeSort\n");
     printf("vetor:");
     for(int i=0;i<6;i++){
-        printf("%i,",vetor4[i]);
+        printf("%i,",vetor5[i]);
     }
-    MergeSort(&vetor4[0],0,5);
+    MergeSort(&vetor5[0],0,5);
     printf("\nvetor ordenado:");
     for(int i=0;i<6;i++){
-        printf("%i,",vetor4[i]);
+        printf("%i,",vetor5[i]);
     }
 }
