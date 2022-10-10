1ba125364f3a5851080b6618d92e7c8937d34d04
Hilster Barbosa Santos <69377592+Hilster00@users.noreply.github.com>
2022-10-09 22:43:46 -0300

    Create BorderSort.py

---
+++ Python/BorderSort.py
@@ -0,0 +1,13 @@
+def BorderSort(vetor,inicio=0,fim=None):
+    if fim == None:
+        fim=len(vetor)-1
+    for k,i in enumerate(range(inicio,fim+1)):
+        posicao_menor=i
+        posicao_maior=fim-k
+        for j in range(i,fim+1-k):
+            if vetor[j] < vetor[posicao_menor]:
+                posicao_menor=j
+            elif vetor[j] > vetor[posicao_maior]:
+                posicao_maior=j
+        vetor[i],vetor[posicao_menor] = vetor[posicao_menor],vetor[i]
+        vetor[fim-k],vetor[posicao_maior] = vetor[posicao_maior],vetor[fim-k]
